7c550eda184be64b0aa372eb7229050c2c484fb5
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-02 22:22:53 -0300

    Update 2.0.py

--- 2.0.py
+++ 2.0.py
@@ -19,7 +19,7 @@ if st.sidebar.button("Haz cick hay una sorpresa"):
 user_input = st.sidebar.text_input("Escribe algo en la barra: ")
 st.sidebar.write(f"Has escrito: {user_input}")
 
-ploaded_file = st.file_uploader("Base.xlsx", type=["xlsx"])
+uploaded_file = st.file_uploader("Base.xlsx", type=["xlsx"])
 
 if uploaded_file is not None:
     # Leer el archivo de Excel
@@ -29,26 +29,10 @@ if uploaded_file is not None:
     st.write("Datos de la base de datos:")
     st.dataframe(df)
 
-    # Barra lateral para seleccionar el tipo de gráfico
-    chart_type = st.sidebar.selectbox("Selecciona el tipo de gráfico:", ("Gráfico de Barras", "Gráfico de Torta"))
-
-    # Mostrar el gráfico según la selección
-    if chart_type == "Gráfico de Barras":
-        st.subheader("Gráfico de Barras")
-        fig, ax = plt.subplots()
-        df.set_index('Categoría').plot(kind='bar', ax=ax)
-        ax.set_ylabel("Número de Goles")
-        ax.set_title("Goles de Cristiano Ronaldo y Lionel Messi")
-        st.pyplot(fig)
-
-    elif chart_type == "Gráfico de Torta":
-        st.subheader("Gráfico de Torta")
-        # Crear gráficos de torta para cada sección
-        for index, row in df.iterrows():
-            labels = ['Cristiano Ronaldo', 'Lionel Messi']
-            sizes = [row['Cristiano Ronaldo'], row['Lionel Messi']]
-            fig, ax = plt.subplots()
-            ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-            st.write(f"Distribución de {row['Categoría']}:")
-            st.pyplot(fig)
+    # Mostrar el gráfico de barras
+    st.subheader("Gráfico de Barras")
+    fig, ax = plt.subplots()
+    df.set_index('Categoría').plot(kind='bar', ax=ax)
+    ax.set_ylabel("Número de Goles")
+    ax.set_title("Goles de Cristiano Ronaldo y Lionel Messi")
+    st.pyplot(fig)
