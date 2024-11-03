266027007ed142c940c865d1a8f7c204d5393280
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-02 22:02:26 -0300

    Update 2.0.py

--- 2.0.py
+++ 2.0.py
@@ -12,3 +12,9 @@ st.image(image, caption='Imagen de CR7', use_column_width=True)
 st.sidebar.title("Barra lateral")
 st.sidebar.header("Hola")
 st.sidebar.write("Esto es mi barra lateral")
+
+if st.sidebar.button("Haz echo cick pero en la barra lateral"):
+    st.sidebar.write("Haz echo click en el botón de la barra lateral")
+
+user_input = st.sidebar.text_input("Escribe algo en la barra: ")
+st.sidebar.write(f"Has escrito: {user_input}")
