8f9b65f6baf8d8763f9a19d382ad0c311d2fe7d1
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-02 22:25:45 -0300

    Update 2.0.py

--- 2.0.py
+++ 2.0.py
@@ -19,20 +19,24 @@ if st.sidebar.button("Haz cick hay una sorpresa"):
 user_input = st.sidebar.text_input("Escribe algo en la barra: ")
 st.sidebar.write(f"Has escrito: {user_input}")
 
-uploaded_file = st.file_uploader("Base.xlsx", type=["xlsx"])
+uploaded_file = st.file_uploader("Elige un archivo Excel (Base de datos cr7 messi)", type=["xlsx"])
 
 if uploaded_file is not None:
-    # Leer el archivo de Excel
-    df = pd.read_excel(uploaded_file)
+    try:
+        # Leer el archivo de Excel
+        df = pd.read_excel(uploaded_file)
 
-    # Mostrar el DataFrame
-    st.write("Datos de la base de datos:")
-    st.dataframe(df)
+        # Mostrar el DataFrame
+        st.write("Datos de la base de datos:")
+        st.dataframe(df)
 
-    # Mostrar el gráfico de barras
-    st.subheader("Gráfico de Barras")
-    fig, ax = plt.subplots()
-    df.set_index('Categoría').plot(kind='bar', ax=ax)
-    ax.set_ylabel("Número de Goles")
-    ax.set_title("Goles de Cristiano Ronaldo y Lionel Messi")
-    st.pyplot(fig)
+        # Mostrar el gráfico de barras
+        st.subheader("Gráfico de Barras")
+        fig, ax = plt.subplots()
+        df.set_index('Categoría').plot(kind='bar', ax=ax)
+        ax.set_ylabel("Número de Goles")
+        ax.set_title("Goles de Cristiano Ronaldo y Lionel Messi")
+        st.pyplot(fig)
+
+    except Exception as e:
+        st.error(f"Error al leer el archivo: {e}")
