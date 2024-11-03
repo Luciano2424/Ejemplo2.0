3d2794cb7ec14267fa74915dba41a38b963b9cc8
Luciano2424 <emprendedores.cl24@gmail.com>
2024-11-02 21:57:16 -0300

    Update 2.0.py

--- 2.0.py
+++ 2.0.py
@@ -4,6 +4,11 @@ from PIL import Image
 # Título de la página
 st.title("Bienvenidos a Universidad San Sebastián")
 
-# Cargar y mostrar la imagen
+#Imagen cr7
 image = Image.open("cr7.jpg")
 st.image(image, caption='Imagen de CR7', use_column_width=True)
+
+#barra lateral
+st.sidebar.title("Barra lateral")
+st.sidebar.header("Hola")
+st.sidebar.write("Esto es mi barra lateral")
